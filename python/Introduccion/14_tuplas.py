fd6e05fc8ece6a16161dcab648248b396aa647bc
ldcalvopina <david.cgranda99@gmail.com>
2024-10-30 12:05:25 -0500

    archivos actualizados

---
+++ python/Introduccion/14_tuplas.py
@@ -0,0 +1,10 @@
+lenguajes = ("Python", "c++", "go", "Javascript") 
+print(type(lenguajes))
+
+print(lenguajes[1])
+print(lenguajes[2:])
+print(lenguajes[:])
+
+print("c++" in lenguajes) #si c++ esta dentro de la tupla lenguajes
+
+print(len(lenguajes))
---
+++ python/Introduccion/15_conjuntos.py
@@ -0,0 +1,10 @@
+lenguajes = {"Python", "c++", "go","c++", "Javascript"}
+print(type(lenguajes))
+
+print(lenguajes)
+
+lenguajes.add("Luis")
+print(lenguajes)
+
+lenguajes2 = lenguajes.copy
+print(lenguajes2)
