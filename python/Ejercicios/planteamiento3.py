c164e73d181705771a393ab36df8742db04f9b8e
ldcalvopina <david.cgranda99@gmail.com>
2024-10-31 15:29:38 -0500

    add plantamiento3.py

---
+++ python/Ejercicios/planteamiento3.py
@@ -0,0 +1,11 @@
+
+numero1 = float(input("Ingrese numero 1: "))
+numero2 = float(input("Ingrese numero 2: "))
+
+suma=numero1+numero2
+valor_restado= suma - 5
+if suma>=25:
+    print("La suma es: ", suma)
+    print("El valor restado 5 es: ", valor_restado)
+elif valor_restado<=10:
+    print("Gracias por su visita")
